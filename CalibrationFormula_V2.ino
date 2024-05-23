a07a5bb8af9cb3da5fdf5afe52e6af485250de07
rifauzh <rifafauziah.rf15@gmail.com>
2024-05-23 20:02:06 +0800

    v2_1

---
+++ CalibrationFormula_V2.ino
@@ -0,0 +1,23 @@
+const float R2 = 100000; // R2 in ohms
+const float Vin = 12.0;  // Vin in volts
+const float Vref = 3.0;  // Vref in volts
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+      float Vout = Serial.readStringUntil('\n').toFloat();
+      float Rdut = calculateRdut(Vin, Vref, Vout, R2);
+      Serial.print("Vout: ");
+      Serial.println(Vout,3);
+      Serial.print("Rdut: ");
+      Serial.println(Rdut);
+
+}
+
+float calculateRdut(float Vin, float Vref, float Vout, float R2) {
+  return (R2 * (Vin - Vref)) / (Vref - Vout);
+}
